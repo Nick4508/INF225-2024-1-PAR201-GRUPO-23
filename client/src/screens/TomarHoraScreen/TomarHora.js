4ef2639ded4bbf1ce42a6b117450a8ab6cfb0e23
MatiasBV <matias.barrerav@usm.cl>
2023-10-29 22:46:05 -0300

    si, tomarhora
    si, ta weno falta el href a la page principal

--- client/src/screens/TomarHoraScreen/TomarHora.js
+++ client/src/screens/TomarHoraScreen/TomarHora.js
@@ -1,62 +1,141 @@
-import React from 'react';
-import {Link} from "react-router-dom"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const TomarHora = () => {
+    const [citaInfo, setCitaInfo] = useState({
+        rut: '',
+        tipoExamen: '',
+        fecha: '',
+        hora: ''
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setCitaInfo({ ...citaInfo, [name]: value });
+    };
+
+    const handleSolicitarCita = (e) => {
+        e.preventDefault();
+        if (!citaInfo.rut || !citaInfo.tipoExamen) {
+            alert("Por favor complete el RUT y el Tipo de Examen.");
+        } else {
+            const script = `
+                <script>
+                rut = '${citaInfo.rut}';
+                tipoExamen = '${citaInfo.tipoExamen}';
+                fecha = '${citaInfo.fecha}';
+                hora = '${citaInfo.hora}';
+                </script>`;
+            alert('Script de JavaScript:\n' + script);
+        }
+    };
+
     return (
         <div className="container">
             <h2>Solicitud de Cita Médica</h2>
-            <form action="" method="post">
+            <form onSubmit={handleSolicitarCita}>
                 <div className="mb-3">
                     <label htmlFor="rut" className="form-label">RUT (sin puntos ni guión):</label>
-                    <input type="text" id="rut" name="rut" pattern="\d{7,8}" title="Ingrese un RUT válido" className="form-control" required />
-                </div>
-
-                <div className="mb-3">
-                    <label htmlFor="fecha" className="form-label">Fecha de la Cita:</label>
-                    <input type="date" id="fecha" name="fecha" className="form-control" required />
-                </div>
-                <label className="form-label">Tipo de Examen:</label>
-                <div className='mb-3 d-flex'>
-                    
-                    <div className='me-3'>
-                        <label>
-                            Scanner 
-                            <input className="ms-1" type="radio" name="type"
-                                id="scanner" value="other" />
-                        </label>
-                    </div>
-                    <div className='me-3'>
-                        <label>
-                            Resonancia Magnética 
-                            <input className="ms-1" type="radio" name="type"
-                                id="resonancia" value="other" />
-                        </label>
-                    </div>
-                    <div className='me-3'>
-                        <label>
-                            Ecografía 
-                            <input className="ms-1" type="radio" name="type"
-                                id="ecografia" value="other" />
-                        </label>
-                    </div>
-                    <div className='me-3'>
-                        <label>
-                            Radiografía 
-                            <input className="ms-1" type="radio" name="type"
-                                id="radiografia" value="other" />
-                        </label>
-                    </div>
+                    <input
+                        type="text"
+                        id="rut"
+                        name="rut"
+                        pattern="[0-9]{7,8}" 
+                        title="Ingrese un RUT válido"
+                        className="form-control"
+                        required
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="hora" className="form-label">Hora de la Cita:</label>
-                    <input type="time" id="hora" name="hora" className="form-control" required />
+                    <label className="form-label">Tipo de Examen:</label>
+                    <div className='mb-3 d-flex'>
+                        <div className='me-3'>
+                            <label>
+                                Scanner
+                                <input
+                                    className="ms-1"
+                                    type="radio"
+                                    name="tipoExamen"
+                                    value="Scanner"
+                                    onChange={handleInputChange}
+                                />
+                            </label>
+                        </div>
+                        <div className='me-3'>
+                            <label>
+                                Resonancia Magnética
+                                <input
+                                    className="ms-1"
+                                    type="radio"
+                                    name="tipoExamen"
+                                    value="Resonancia Magnética"
+                                    onChange={handleInputChange}
+                                />
+                            </label>
+                        </div>
+                        <div className='me-3'>
+                            <label>
+                                Ecografía
+                                <input
+                                    className="ms-1"
+                                    type="radio"
+                                    name="tipoExamen"
+                                    value="Ecografía"
+                                    onChange={handleInputChange}
+                                />
+                            </label>
+                        </div>
+                        <div className='me-3'>
+                            <label>
+                                Radiografía
+                                <input
+                                    className="ms-1"
+                                    type="radio"
+                                    name="tipoExamen"
+                                    value="Radiografía"
+                                    onChange={handleInputChange}
+                                />
+                            </label>
+                        </div>
+                    </div>
                 </div>
-
-                <button type="submit" className="btn btn-primary">Solicitar Cita</button>
+                <div className="mb-3">
+                    <label htmlFor="fecha" className="form-label">
+                        Fecha de la Cita:
+                    </label>
+                    <input
+                        type="date"
+                        id="fecha"
+                        name="fecha"
+                        className="form-control"
+                        required
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="hora" className="form-label">
+                        Hora de la Cita:
+                    </label>
+                    <input
+                        type="time"
+                        id="hora"
+                        name="hora"
+                        className="form-control"
+                        required
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                    Solicitar Cita
+                </button>
             </form>
-            <br></br>
-
-            <Link to="/"><button type="submit" className='btn btn-primary'>Volver</button></Link>
-
+            <br />
+            <Link to="/">
+                <button type="submit" className="btn btn-primary">
+                    Volver
+                </button>
+            </Link>
         </div>
     );
 };
