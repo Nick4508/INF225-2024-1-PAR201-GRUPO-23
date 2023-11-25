6ff2ba7dbf99f5ce56acc95a17c71456cd998585
Nick4508 <nicolas.rodriguezbe@sansano.usm.cl>
2023-11-25 19:28:55 -0300

    actualizacion

--- client/src/App.js
+++ client/src/App.js
@@ -14,36 +14,63 @@ registerLocale("es", es);
 
 function App() {
 	const [fechaSeleccionada, setFechaSeleccionada] = useState(new Date());
-
+	const [opcionSeleccionada, setOpcionSeleccionada] = useState('radiografias');
+	const handleOpcionChange = (opcion) => {
+		setOpcionSeleccionada(opcion);
+	  };
 	const handleFechaChange = (date) => {
 		setFechaSeleccionada(date);
 	};
+
+	const renderPrincipalComponent = () => {
+		switch (opcionSeleccionada) {
+		  case 'radiografias':
+			return <PrincipalRadiografias fechaSeleccionada={fechaSeleccionada} />;
+		  case 'scanners':
+			return <PrincipalScanners fechaSeleccionada={fechaSeleccionada} />;
+		  case 'ecografias':
+			return <PrincipalEcografias fechaSeleccionada={fechaSeleccionada} />;
+		  case 'resonancias':
+			return <PrincipalResonancias fechaSeleccionada={fechaSeleccionada} />;
+		  default:
+			return null;
+		}
+	}
 	return (
-	
+
+		
 	<div>
 		<Navbar />
-		<div class="row mt-4">
-			<div class="col-4 d-flex flex-column align-items-center">
-				<h5>Selecciona la fecha de los examenes</h5>
-				<DatePicker selected={fechaSeleccionada} onChange={handleFechaChange} locale="es" dateFormat="dd-MM-yyyy" />
-			</div>
-			<div className='principal' class="col-8">
-				<ul style={{ listStyle: 'none', padding: 0 }}>
-					<li>
-						<PrincipalRadiografias fechaSeleccionada={fechaSeleccionada} />
-					</li><br/>
-					<li>
-						<PrincipalScanners fechaSeleccionada={fechaSeleccionada} />
-					</li><br/>
-					<li>
-						<PrincipalEcografias fechaSeleccionada={fechaSeleccionada}/>
-					</li><br/>
-					<li>
-						<PrincipalResonancias fechaSeleccionada={fechaSeleccionada}/>
-					</li>
-				</ul>
-				<br/>
-			</div>
+		<div className="row mt-4 mr-3">
+        <div className="col-md-6">
+          <div className="d-flex flex-column align-items-center">
+            <h5>Selecciona la fecha de los exámenes</h5>
+            <DatePicker
+              selected={fechaSeleccionada}
+              onChange={handleFechaChange}
+              locale="es"
+              dateFormat="dd-MM-yyyy"
+            />
+          </div>
+        </div>
+        <div className="col-md-6 mr-3">
+          <div className="d-flex flex-column align-items-center">
+            <h5>Selecciona el examen</h5>
+            <select
+              className="form-select"
+              onChange={(e) => handleOpcionChange(e.target.value)}
+              value={opcionSeleccionada}
+            >
+              <option value="radiografias">Radiografías</option>
+              <option value="scanners">Scanners</option>
+              <option value="ecografias">Ecografías</option>
+              <option value="resonancias">Resonancias</option>
+            </select>
+          </div>
+        </div>
+      </div>
+		<div className='principal' class="row mt-4">
+			<div class="row-4 d-flex flex-column align-items-center">{renderPrincipalComponent()}</div>
 		</div>
 	</div>
 
