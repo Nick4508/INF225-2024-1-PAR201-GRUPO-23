f4a5fc08082d91c5309bc0418deefc0c3f852d9b
Nick4508 <nicolas.rodriguezbe@sansano.usm.cl>
2024-04-13 19:39:22 -0400

    database connection

--- server/Database/index.js
+++ server/Database/index.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL = "mongodb+srv://nicolas:admin123@database.6atshhf.mongodb.net/?retryWrites=true&w=majority"
-//admin123
+// const MONGO_URL = "mongodb+srv://nicolas:admin123@database.6atshhf.mongodb.net/?retryWrites=true&w=majority"
+// const MONGO_URL = "mongodb+srv://database.kvxoxaz.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=database"
+const MONGO_URL = "mongodb+srv://nicolas:admin123@database.kvxoxaz.mongodb.net/?retryWrites=true&w=majority&appName=database"
+// const MONGO_URL = "mongodb+srv://database.kvxoxaz.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=database"
 const db = async () => {
     try{
         const conn = await mongoose.connect(MONGO_URL);
